28c1146b70ed10b9947a9822d1c42936de5da6bd
Vaibhav P <vaibhav.martian@gmail.com>
2025-05-19 16:38:38 +0530

    database migration

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,19 +1,21 @@
 import {
-  pgTable,
   integer,
   varchar,
   boolean,
   timestamp,
+  pgTableCreator,
 } from "drizzle-orm/pg-core";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 
-export const tasks = pgTable("tasks", {
+export const createTable = pgTableCreator((name) => `ace_${name}`);
+
+export const tasks = createTable("tasks", {
   id: integer("id").generatedAlwaysAsIdentity().primaryKey(),
   title: varchar("title").notNull(),
   description: varchar("description"),
   completed: boolean("completed").notNull().default(false),
   created_at: timestamp("created_at").defaultNow().notNull(),
-  updated_at: timestamp("updated_at").defaultNow(),
+  updated_at: timestamp("updated_at").defaultNow().notNull(),
   due: timestamp("due"),
   user_id: varchar("user_id").notNull(),
   team_id: varchar("team_id").notNull(),
